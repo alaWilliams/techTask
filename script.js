76fc8193dfb6033d9db29970c455d93f48fd7843
Ala Williams <alicja.m.williams@gmail.com>
2022-06-30 11:34:58 +0200

    linking js file

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.body.innerHTML = '<h1>Just checking</h1>'
 const students = [
 {
 name: 'Martin',
@@ -25,64 +24,25 @@ marks: [9, 8, 9, 7, 5],
 },
 ];
 
-//getting first names
-Document.getElementById('name1') = students[0].name;
-Document.getElementById('name2') = students[1].name;
-Document.getElementById('name3') = students[2].name;
-Document.getElementById('name4') = students[3].name;
-
-//getting last names
-Document.getElementById('last-name1') = students[0].lastName;
-Document.getElementById('last-name2') = students[1].lastName;
-Document.getElementById('last-name3') = students[2].lastName;
-Document.getElementById('last-name4') = students[3].lastName;
-
-//getting array of marks
-let marks1 = students[0].marks;
-let marks2 = students[1].marks;
-let marks3 = students[2].marks;
-let marks4 = students[3].marks;
-
-//returning the quantity of lectures
 function visitedLectures(arr) {
   let newArr = arr.filter(Number);
-  console.log(newArr.length)
-};
-
-//counting the average mark
+  return newArr.length;
+}
+ 
 function averageMark(arr) {
+  let arrWithoutNull = arr.filter(Number);
   let total = 0;
-  for (let i = 0; i < arr.length; i++) {
-  total =+ arr.length;
-  let avg = total / arr.length;
-  console.log(avg);
+  for (let i = 0; i < arrWithoutNull.length; i++) {
+    total =+ arrWithoutNull.length;
+    let avg = total / arrWithoutNull.length;
+    return avg;
+    };
+  return avg
   };
-};
 
-//setting the quantity of lectures
-Document.getElementById('lectures1'){
-  visitedLectures(marks1)
-};
-Document.getElementById('lectures2'){
-  visitedLectures(marks2)
-};
-Document.getElementById('lectures3'){
-  visitedLectures(marks3)
-};
-Document.getElementById('lectures4'){
-  visitedLectures(marks4)
-};
-
-//setting the average grade
-Document.getElementById('average1'){
-  averageMark(marks1)
-};
-Document.getElementById('average2'){
-  averageMark(marks2)
-};
-Document.getElementById('average3'){
-  averageMark(marks3)
-};
-Document.getElementById('average4'){
-  averageMark(marks4)
+for (const student of students) {
+  document.getElementById('name').innerHTML = student.name;
+  document.getElementById('last-name').innerHTML = student.lastName;
+  document.getElementById('lectures').innerHTML = visitedLectures(student.marks);
+  document.getElementById('average').innerHTML = averageMark(student.marks);
 };
